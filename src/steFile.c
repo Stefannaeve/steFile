d5988f2bcaf6569a68104942aa8be55f98137888
Stefan Næve <stefan.naeve@hotmail.com>
2025-02-02 00:54:47 +0100

    Small changes

--- src/steFile.c
+++ src/steFile.c
@@ -11,12 +11,16 @@
 
 typedef struct _STE_FILE {
     char magicValue[5];
-    char *width;
-    char *height;
+    char width;
+    char height;
 } STE_FILE;
 
 void writeToFile(const STE_FILE *steFile, FILE *file);
-int8_t widthAndHeightChecks(const unsigned int iHeightLength, const unsigned int iWidthLength, const char *pszHeight, const char *pszWidth);
+
+int8_t widthAndHeightChecks(unsigned int iHeightLength,
+                            unsigned int iWidthLength,
+                            const char *pszHeight,
+                            const char *pszWidth);
 
 int createFile(const char **argv) {
     int8_t iStatus = 0;
@@ -26,7 +30,7 @@ int createFile(const char **argv) {
     const unsigned int iHeightLength = strlen(pszHeight);
     const unsigned int iWidthLength = strlen(pszWidth);
     unsigned int iLengthOfName = 0;
-    unsigned int checkIfExtentionValue = 1;
+    unsigned int checkIfExtensionValue = 1;
     char *pszFileName;
     char szCheckIfExtension[5];
     FILE *file;
@@ -38,89 +42,80 @@ int createFile(const char **argv) {
         return 1;
     }
 
-    steFile.height = (char *) calloc(iHeightLength + 1, sizeof(char));
-    if (steFile.height == NULL) {
-        iStatus = 1;
-        printf("Memory allocation issue");
+    memcpy(steFile.magicValue, MAGIC_VALUE, 4);
+    steFile.magicValue[4] = '\0';
+
+    steFile.height = atoi(pszHeight);
+    steFile.width = atoi(pszWidth);
+
+    iLengthOfName = strlen(pszName);
+
+    if (iLengthOfName > LENGTH_OF_EXTENSION) {
+        for (int i = 0; i < LENGTH_OF_EXTENSION; i++) {
+            szCheckIfExtension[i] = pszName[iLengthOfName - LENGTH_OF_EXTENSION + i];
+        }
+        szCheckIfExtension[LENGTH_OF_EXTENSION] = '\0';
+
+        checkIfExtensionValue = strncmp(szCheckIfExtension, FILE_EXTENSION, 4);
+    }
+
+    if (checkIfExtensionValue == 0) {
+        if (access(pszName, F_OK) == F_OK) {
+            iStatus = 1;
+            printf("File already exists\n");
+        } else {
+            file = fopen(pszName, "w");
+            if (file == NULL) {
+                iStatus = 1;
+                printf("Couldn't open file");
+            } else {
+                writeToFile(&steFile, file);
+            }
+        }
     } else {
-        steFile.width = (char *) calloc(iWidthLength + 1, sizeof(char));
-        if (steFile.width == NULL) {
+        pszFileName = (char *) calloc(iLengthOfName + LENGTH_OF_EXTENSION + 1,
+                                    sizeof(char));
+
+        if (pszFileName == NULL) {
             iStatus = 1;
             printf("Memory allocation issue");
         } else {
-            memcpy(steFile.magicValue, MAGIC_VALUE, 4);
-            steFile.magicValue[4] = '\0';
+            strncat(pszFileName, pszName, iLengthOfName);
+            strcat(pszFileName, FILE_EXTENSION);
+            pszFileName[iLengthOfName + LENGTH_OF_EXTENSION] = '\0';
 
-            memcpy(steFile.height, pszHeight, iHeightLength);
-            memcpy(steFile.width, pszHeight, iWidthLength);
-
-            iLengthOfName = strlen(pszName);
-
-            if (iLengthOfName > LENGTH_OF_EXTENSION) {
-                for (int i = 0; i < LENGTH_OF_EXTENSION; i++) {
-                    szCheckIfExtension[i] = pszName[iLengthOfName - LENGTH_OF_EXTENSION + i];
-                }
-                szCheckIfExtension[LENGTH_OF_EXTENSION] = '\0';
-
-                checkIfExtentionValue = strncmp(szCheckIfExtension, FILE_EXTENSION, 4);
-            }
-
-            if (checkIfExtentionValue == 0) {
-                if (access(pszName, F_OK) == F_OK) {
-                    iStatus = 1;
-                    printf("File already exists\n");
-                } else {
-                    file = fopen(pszName, "w");
-                    if (file == NULL) {
-                        iStatus = 1;
-                        printf("Couldnt open file");
-                    } else {
-                        writeToFile(&steFile, file);
-                    }
-                }
+            if (access(pszFileName, F_OK) == F_OK) {
+                iStatus = 1;
+                printf("File already exists\n");
             } else {
-                pszFileName = (char *) calloc(iLengthOfName + LENGTH_OF_EXTENSION + 1, sizeof(char));
-
-                if (pszFileName == NULL) {
+                file = fopen(pszFileName, "w");
+                if (file == NULL) {
                     iStatus = 1;
-                    printf("Memory allocation issue");
+                    printf("Couldn't open file");
                 } else {
-                    strncat(pszFileName, pszName, iLengthOfName);
-                    strcat(pszFileName, FILE_EXTENSION);
-                    pszFileName[iLengthOfName + LENGTH_OF_EXTENSION] = '\0';
-
-                    if (access(pszFileName, F_OK) == F_OK) {
-                        iStatus = 1;
-                        printf("File already exists\n");
-                    } else {
-                        file = fopen(pszFileName, "w");
-                        if (file == NULL) {
-                            iStatus = 1;
-                            printf("Couldnt open file");
-                        } else {
-                            writeToFile(&steFile, file);
-                        }
-                    }
-                    free(pszFileName);
+                    writeToFile(&steFile, file);
                 }
             }
-            free(steFile.width);
+            free(pszFileName);
         }
-        free(steFile.height);
     }
+
 
     return iStatus;
 }
 
 void writeToFile(const STE_FILE *steFile, FILE *file) {
     fputs(steFile->magicValue, file);
-    fputs(steFile->width, file);
-    fputs(steFile->height, file);
+    fputc(steFile->width, file);
+    fputc(steFile->height, file);
     fputc('\n', file);
     fclose(file);
 }
 
-int8_t widthAndHeightChecks(const unsigned int iHeightLength, const unsigned int iWidthLength, const char *pszHeight, const char *pszWidth) {
+int8_t widthAndHeightChecks(const unsigned int iHeightLength,
+                            const unsigned int iWidthLength,
+                            const char *pszHeight,
+                            const char *pszWidth) {
     if (iWidthLength > 2 || iHeightLength > 2) {
         printf("Width or height to big, should be between 4-99\n");
         return 1;
