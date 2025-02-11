54b6ab33aa70211dfe5c06e4ba2626d8a1ccf7db
Stefan Næve <stefan.naeve@hotmail.com>
2025-02-11 22:04:12 +0100

    Added explenations for wrong arguments

--- src/main.c
+++ src/main.c
@@ -17,23 +17,32 @@ int main(int argc, const char *argv[]) {
     int iStatus = 0;
 
     logDebug("1");
+    logError("1,5");
 
     if (argc < 2) {
         printf("You have to have more arguments\n");
+        printf("Expected formats:\n");
+        printf("Expected format one: ./main CREA myfile(.txt)\n");
+        printf("Expected format two: ./main READ\n");
         return 1;
     }
 
     logDebug("2");
 
-    iFirstArgLength = strlen(argv[1]);
+    iFirstArgLength = strnlen(argv[1], FIRST_ARGUMENT_LENGTH+1);
 
     if (iFirstArgLength != FIRST_ARGUMENT_LENGTH) {
         printf("Wrong length on first argument\n");
+        printf("Expected formats:\n");
+        printf("Expected format one: CREA\n");
+        printf("Expected format two: READ\n");
         return 1;
     }
 
     if (strncmp(argv[1], CREA, 4) == 0){
         if (argc < 3) {
+            printf("Wrong arguments\n");
+            printf("Expected format: ./main CREA myfile(.txt)\n");
             return 1;
         }
         iStatus = createFile(argv);
