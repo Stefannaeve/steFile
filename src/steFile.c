2a604174e478810e25b1358c9cb016fa955a0242
Stefan Næve <stefan.naeve@hotmail.com>
2025-04-08 20:00:32 +0200

    Add functionality to use rawImage path in the current setup. Need to fix some memory issues in CreateFile function

--- src/steFile.c
+++ src/steFile.c
@@ -145,9 +145,23 @@ int createFile2(const char **argv) {
     return iStatus;
 }
 
+char *allocateMemoryForFolderName(char *name, int iNameLength, int iFolderNameLength, char *folderName,
+                                  const char *pszName) {
+    name = calloc(iNameLength + iFolderNameLength + 1, sizeof(
+                      char));
+    if (name == NULL) {
+        logError("Failed to allocate memory");
+        printf("Failed to allocate memory\n");
+    } else {
+        strncat(name, folderName, iFolderNameLength);
+        strncat(name, pszName, iNameLength);
+        name[iNameLength + iFolderNameLength] = '\0';
+    }
+    return name;
+}
+
 int readTextFile(STE_FILE *steFile, const char *pszName) {
     int iStatus = 0;
-    int position = 0;
     const char *delimiter = " ";
     char *width;
     char *height;
@@ -159,109 +173,124 @@ int readTextFile(STE_FILE *steFile, const char *pszName) {
     char buffer[BUFFER_SIZE] = {0};
     char *folderName = "rawImageFiles/";
     int iNameLength = strlen(pszName);
+
     int iFolderNameLength = strlen(folderName);
-    char *name = calloc(iNameLength + iFolderNameLength + 1, sizeof(
-                            char));
+
+    char *name = NULL;
+
+    if (iNameLength > iFolderNameLength) {
+        if (!strncmp(pszName, folderName, iNameLength)) {
+            name = allocateMemoryForFolderName(name, iNameLength, iFolderNameLength, folderName, pszName);
+            if (name == NULL) {
+                iStatus = 1;
+                return iStatus;
+            }
+        } else {
+            name = calloc(iNameLength + 1, sizeof(
+                              char));
+            if (name == NULL) {
+                iStatus = 1;
+                logError("Failed to allocate memory");
+                printf("Failed to allocate memory\n");
+            } else {
+                strncpy(name, pszName, iNameLength);
+                name[iNameLength] = '\0';
+            }
+        }
+    } else {
+        name = allocateMemoryForFolderName(name, iNameLength, iFolderNameLength, folderName, pszName);
+        if (name == NULL) {
+            iStatus = 1;
+            return iStatus;
+        }
+    }
+
     uint8_t *bodyBuffer;
 
-    if (name == NULL) {
+    if (access(name, F_OK) != F_OK) {
         iStatus = 1;
-        logError("Failed to allocate memory");
-        printf("Failed to allocate memory\n");
+        printf("File \"%s\" doesnt exist\n", name);
     } else {
-        strncat(name, folderName, iFolderNameLength);
-        strncat(name, pszName, iNameLength);
-
-        if (access(name, F_OK) != F_OK) {
+        FILE *file = fopen(name, "r");
+        if (file == NULL) {
             iStatus = 1;
-            printf("File \"%s\" doesnt exist\n", name);
+            logError("Failed to open file: %s", name);
+            printf("Failed to open file: %s", name);
         } else {
-            FILE *file = fopen(name, "r");
-            if (file == NULL) {
+            if (fgets(buffer, BUFFER_SIZE, file) == NULL) {
+                logError("Buffer is null, failed fgets\n");
                 iStatus = 1;
-                logError("Failed to open file: %s", name);
-                printf("Failed to open file: %s", name);
-            } else {
-                if (fgets(buffer, BUFFER_SIZE, file) == NULL) {
-                    logError("Buffer is null, failed fgets\n");
-                    iStatus = 1;
-                }
-                width = strtok(buffer, delimiter);
-                printf("%s", width);
-                height = strtok(NULL, delimiter);
-                printf(":%s\n", height);
-                iHeightLength = strlen(height);
-                printf("Length of height: %d\n", iHeightLength);
+            }
+            width = strtok(buffer, delimiter);
+            printf("%s", width);
+            height = strtok(NULL, delimiter);
+            printf(":%s\n", height);
+            iHeightLength = strlen(height);
+            printf("Length of height: %d\n", iHeightLength);
 
-                height[iHeightLength - 1] = '\0';
+            height[iHeightLength - 1] = '\0';
 
-                iWidth = strtol(width, &endpointer, 10);
-                if (endpointer == width) {
-                    iStatus = 1;
-                    printf("No digits were found.\n");
-                    return iStatus;
-                }
-                if (*endpointer != '\0') {
-                    iStatus = 1;
-                    printf("Invalid character: %c\n", *endpointer);
-                    return iStatus;
-                }
+            iWidth = strtol(width, &endpointer, 10);
+            if (endpointer == width) {
+                iStatus = 1;
+                printf("No digits were found.\n");
+                return iStatus;
+            }
+            if (*endpointer != '\0') {
+                iStatus = 1;
+                printf("Invalid character: %c\n", *endpointer);
+                return iStatus;
+            }
 
-                printf("The width is: %ld\n", iWidth);
-                // *endpointer = 0;
+            printf("The width is: %ld\n", iWidth);
 
-                iHeight = strtol(height, &endpointer, 10);
-                if (endpointer == width) {
-                    iStatus = 1;
-                    printf("No digits were found.\n");
-                    return iStatus;
-                }
-                if (*endpointer != '\0') {
-                    iStatus = 1;
-                    printf("Invalid character: %c\n", *endpointer);
-                    return iStatus;
-                }
+            iHeight = strtol(height, &endpointer, 10);
+            if (endpointer == width) {
+                iStatus = 1;
+                printf("No digits were found.\n");
+                return iStatus;
+            }
+            if (*endpointer != '\0') {
+                iStatus = 1;
+                printf("Invalid character: %c\n", *endpointer);
+                return iStatus;
+            }
 
-                printf("The height is: %ld\n", iHeight);
+            printf("The height is: %ld\n", iHeight);
 
-                steFile->width = iWidth;
-                steFile->height = iHeight;
+            steFile->width = iWidth;
+            steFile->height = iHeight;
 
-                steFile->body = calloc((iWidth * iHeight), sizeof(uint8_t));
-                bodyBuffer = calloc(iWidth + 1, sizeof(uint8_t));
+            steFile->body = calloc((iWidth * iHeight), sizeof(uint8_t));
+            bodyBuffer = calloc(iWidth + 1, sizeof(uint8_t));
 
-                uint8_t character = 0;
+            uint8_t character = 0;
 
-                for (int i = 0; i < iHeight; i++) {
-                    memset(bodyBuffer, 0, iWidth + 1);
-                    for (int j = 0; j < iWidth; j++) {
-                        character = getc(file);
-                        //logDebug("%d: character representation: %c", j, character);
-                        if (character == '1') {
-                            character = 0xFF;
-                        } else {
-                            character -= 48;
-                        }
-                        //logDebug("%d, character: %d", j, character);
-                        bodyBuffer[j] = character;
+            for (int i = 0; i < iHeight; i++) {
+                memset(bodyBuffer, 0, iWidth + 1);
+                for (int j = 0; j < iWidth; j++) {
+                    character = getc(file);
+                    if (character == '1') {
+                        character = 0xFF;
+                    } else {
+                        character -= 48;
                     }
-                    //fgets(bodyBuffer, iWidth + 1, file);
-                    //logDebug("buffer%d: %s", i, bodyBuffer);
-                    //strncat(steFile->body, bodyBuffer, iWidth);
-                    for (int j = 0; j < iWidth; j++) {
-                        steFile->body[i * iWidth + j] = bodyBuffer[j];
-                    }
-                    //logDebug("Stefile%d: %s", i, steFile->body);
-                    while ((character = getc(file)) != '\n') {
-                        if (character == EOF || character == 255) {
-                            break;
-                        }
+                    bodyBuffer[j] = character;
+                }
+                for (int j = 0; j < iWidth; j++) {
+                    steFile->body[i * iWidth + j] = bodyBuffer[j];
+                }
+                while ((character = getc(file)) != '\n') {
+                    if (character == EOF || character == 255) {
+                        break;
                     }
                 }
             }
         }
-        free(name);
     }
+    free(name);
+
+
     return iStatus;
 }
 
@@ -274,26 +303,19 @@ int createFile(const char **argv) {
     unsigned int checkIfExtensionValue = 1;
     char *pszFileName;
     char *imageFolder = "images/";
+    char *rawImageFolderName = "rawImageFiles/";
+    int rawImageFolderLength = strlen(rawImageFolderName);
     int imageFolderLength = strlen(imageFolder);
     char szCheckIfExtension[5];
     FILE *file;
     STE_FILE steFile;
 
-    logDebug("Hei");
+    int originalFolderNameLength = strlen(pszName);
 
-    //iStatus = widthAndHeightChecks(iHeightLength, iWidthLength, pszHeight, pszWidth);
-
-    /*
-    if (iStatus != 0) {
-        return 1;
-    }
-    */
+    char *name = NULL;
 
     memcpy(steFile.magicValue, MAGIC_VALUE, 4);
     steFile.magicValue[4] = '\0';
-
-    //steFile.height = atoi(pszHeight);
-    //steFile.width = atoi(pszWidth);
 
     iStatus = readTextFile(&steFile, pszName);
 
@@ -301,11 +323,31 @@ int createFile(const char **argv) {
         return iStatus;
     }
 
-    iLengthOfName = strlen(pszName);
+
+    // Check if the prefix "rawImageFiles/" is here, if it is, remove it
+    if (rawImageFolderLength < originalFolderNameLength) {
+        if (strncmp(pszName, rawImageFolderName, rawImageFolderLength) == 0) {
+            name = calloc(originalFolderNameLength - rawImageFolderLength + 1, sizeof(char));
+            for (int i = rawImageFolderLength; i < originalFolderNameLength; i++) {
+                name[i-rawImageFolderLength] = pszName[i];
+            }
+            name[originalFolderNameLength - rawImageFolderLength] = '\0';
+        } else {
+            name = calloc(originalFolderNameLength + 1, sizeof(char));
+            strncpy(name, pszName, originalFolderNameLength);
+            name[originalFolderNameLength] = '\0';
+        }
+    } else {
+        name = calloc(originalFolderNameLength + 1, sizeof(char));
+        strncpy(name, pszName, originalFolderNameLength);
+        name[originalFolderNameLength] = '\0';
+    }
+
+    iLengthOfName = strlen(name);
 
     if (iLengthOfName > LENGTH_OF_EXTENSION) {
         for (int i = 0; i < LENGTH_OF_EXTENSION; i++) {
-            szCheckIfExtension[i] = pszName[iLengthOfName - LENGTH_OF_EXTENSION + i];
+            szCheckIfExtension[i] = name[iLengthOfName - LENGTH_OF_EXTENSION + i];
         }
         szCheckIfExtension[LENGTH_OF_EXTENSION] = '\0';
 
@@ -321,7 +363,7 @@ int createFile(const char **argv) {
             printf("Memory allocation issue");
         } else {
             strncat(pszFileName, imageFolder, imageFolderLength);
-            strncat(pszFileName, pszName, iLengthOfName - 4);
+            strncat(pszFileName, name, iLengthOfName - 4);
             strncat(pszFileName, FILE_EXTENSION, LENGTH_OF_EXTENSION + 1);
             pszFileName[iLengthOfName - 4 + LENGTH_OF_EXTENSION + imageFolderLength] = '\0';
         }
@@ -339,6 +381,8 @@ int createFile(const char **argv) {
         }
     }
 
+    logDebug("Images Folder Name: %s", pszFileName);
+
     file = fopen(pszFileName, "w");
     if (file == NULL) {
         iStatus = 1;
@@ -347,6 +391,7 @@ int createFile(const char **argv) {
         writeToFile(&steFile, file);
     }
     free(pszFileName);
+    free(name);
 
 
     return iStatus;
